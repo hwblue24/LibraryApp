195200ce25d3fc32fec1997ba1cbf1a507d5ddd9
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2024-06-29 13:24:24 -0400

    Manipulate DOM to create divs for books

--- lib.js
+++ lib.js
@@ -4,34 +4,33 @@ function Book(title, author, pages, read){
     this.title = title; 
     this.author=author; 
     this.pages= pages; 
-    this.read = read; 
-    this.info = function () { 
-        return (`${this.title}, written by ${this.author}, has ${this.pages}, and you have ${this.read} it`)
-    }
+    this.read = read;
 }
 
+//prototype defined on prototype for efficiency 
+Book.prototype.info = function () { 
+    return (`${this.title}, written by ${this.author}, has ${this.pages}, and you have ${this.read} it`)
+};
 
 
-function addBookToLibrary (title, author, pages, read) {
+function addBookToLibrary () {
     title = prompt('Provide title of book');
     author = prompt('Provide author\'s name');
     pages = prompt('Provide length of book in pages'); 
-    read = prompt(`Have you read the book? Input "read" or "not read" `);
-    title = new Book (title,author,pages,read); 
-    myLibrary.push(title);
+    read = prompt(`Have you read the book?" `);
+    newBook = new Book (title,author,pages,read); 
+    myLibrary.push(newBook);
+    return myLibrary
 
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+function bookTables(myLibrary) { 
+    for (let books of myLibrary) {
+        let container = document.querySelector(".container");
+        content = document.createElement("div");
+        content.textContent = `Title: ${books.title}, Author:${books.author}, Pages: ${books.pages}, Read: ${books.read}`;
+        container.appendChild(content);
 
-
-
-
-
-/*const theLordOfTheRings = new Book ('The Lord of The Rings', 'JK Row', 130, 'read');
-
-console.log(theLordOfTheRings)
-
-console.log(theLordOfTheRings.info());*/
+    }
+}
 
